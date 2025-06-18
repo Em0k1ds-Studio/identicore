2050b265a37d06412ccda04412d67e26e12dcbd3
quyxishi <78678868+quyxishi@users.noreply.github.com>
2025-06-18 17:20:48 +0300

    chore(identicore): updated stubs and added `compute-detection-accuracy` cmd

--- stubs/identicore/types.pyi
+++ stubs/identicore/types.pyi
@@ -1,14 +1,23 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Literal
 
-from _typeshed import Incomplete
+from numpy import dtype, floating, ndarray
+from numpy._typing._nbit_base import _32Bit
 
-InspireModel: Incomplete
+InspireModel: Literal['Pikachu', 'Megatron']
 ImagePath = Path | str
-FaceFeaturesArray: Incomplete
+FaceFeaturesArray: ndarray[tuple[int], dtype[floating[_32Bit]]]
 
 @dataclass
 class FaceComparisonResult:
     is_match: bool
     similarity_confidence: float
-    def __init__(self, is_match, similarity_confidence) -> None: ...
+
+@dataclass
+class DrawingOpts:
+    draw_boxes: bool = ...
+    boxes_label: str = ...
+    draw_landmarks: bool = ...
+
+DefaultDrawingOpts: DrawingOpts
